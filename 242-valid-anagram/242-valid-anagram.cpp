bcc6ffbd7e0552938b863753db65834f629e9d07
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-07-09 13:59:44 +0530

    Time: 15 ms (53.12%), Space: 7.5 MB (7.87%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        //below condition is to get longest string
+        if(s.size() < t.size()) return isAnagram(t,s);
+
+        map<char,int> mp;
+        for(int i=0; i<s.size(); ++i) mp[s[i]]++;
+        
+        for(int i=0; i<t.size(); ++i) mp[t[i]]--;
+        for(auto it:mp) if(it.second > 0) return false; 
+                
+        return true;
+    }
+};
