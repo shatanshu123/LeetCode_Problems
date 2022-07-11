44e2a449a9d3c1a2fbcbda5c1ef5e101aee6f670
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-07-11 13:59:40 +0530

    STRIVER_SDE_SHEET

---
+++ Repeat and Missing Number.cpp
@@ -0,0 +1,23 @@
+vector<int> Solution::repeatedNumber(const vector<int> &A)
+{
+int n = A.size();
+long int actualSum = 0;
+long int Sum1ToN = long(n*long(n+1))/2;
+long int Sum1ToN2 = long(n*long(n+1)*long(2*n+1))/6;
+long int actualSumOfSquares = 0;
+
+for(int i = 0;i<n;i++)
+{
+actualSum += A[i];
+actualSumOfSquares += long(A[i])*long(A[i]);
+}
+
+long int a_minus_b = actualSum - Sum1ToN;
+long int a2_minus_b2 = actualSumOfSquares - Sum1ToN2;
+long int a_plus_b = a2_minus_b2/a_minus_b;
+
+long int a = long(a_minus_b +a_plus_b)/2;
+long int b = a_plus_b - a;
+
+return {a,b};
+}
