d3dbb45898d239fec7f25a6dd12c0d7cc7b694a7
SHATANSHU AGARWAL <shatanshu.1923csi1044@kiet.edu>
2022-07-13 22:52:06 +0530

    Time: 57 ms (55.67%), Space: 36.1 MB (69.88%) - LeetHub

---
+++ 485-max-consecutive-ones/485-max-consecutive-ones.cpp
@@ -0,0 +1,18 @@
+//STRIVER_SDE_SHEET
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int res=0,curr=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==0)
+                curr=0;
+            else{
+                curr++;
+                res=max(res,curr);
+            }
+                
+        }
+        return res;
+    }
+};
